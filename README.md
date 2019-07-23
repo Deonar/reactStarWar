346530c416fb2f90170d646b134b3cf7c13ef0dc
Deonar <deonarius@gmail.com>
2019-07-23 17:06:05 +0300

    finish app

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-Todo Application
------
+CONTEXT
+--------
+
+<App>
+  <Provider value={chatService}>
+    <HomePage>
+      <Dashboard>
+        <NewNotifications>
+        </NewNotifications>
+      </Dashboard>
+    </HomePage>
+  
+    <SupportPage>
+      <MyOpenTickets />
+      <TeamSpace>
+        <Chat>
+        </Chat>
+      </TeamSpace>
+    </SupportPage>
+  </Provider>
+</App>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,138 +1,94 @@
 import React, { Component } from 'react';
 
-import AppHeader from '../app-header';
-import TodoList from '../todo-list';
-import SearchPanel from '../search-panel';
-import ItemStatusFilter from '../item-status-filter';
-import ItemAddForm from '../item-add-form';
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ErrorBoundry from '../error-boundry';
+import SwapiService from '../../services/swapi-service';
+import DummySwapiService from '../../services/dummy-swapi-service';
+
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage } from '../pages';
+
+import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import StarshipDetails from '../sw-components/starship-details';
 
 export default class App extends Component {
 
-  maxId = 100;
-
   state = {
-    items: [
-      { id: 1, label: 'Drink Coffee', important: false, done: false },
-      { id: 2, label: 'Learn React', important: true, done: false },
-      { id: 3, label: 'Make Awesome App', important: false, done: false }
-    ],
-    filter: 'all',
-    search: ''
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   };
 
-  onItemAdded = (label) => {
-    this.setState((state) => {
-      const item = this.createItem(label);
-      return { items: [...state.items, item] };
-    })
-  };
-
-  toggleProperty = (arr, id, propName) => {
-    const idx = arr.findIndex((item) => item.id === id);
-    const oldItem = arr[idx];
-    const value = !oldItem[propName];
-
-    const item = { ...arr[idx], [propName]: value } ;
-    return [
-      ...arr.slice(0, idx),
-      item,
-      ...arr.slice(idx + 1)
-    ];
-  };
-
-  onToggleDone = (id) => {
-    this.setState((state) => {
-      const items = this.toggleProperty(state.items, id, 'done');
-      return { items };
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
     });
   };
 
-  onToggleImportant = (id) => {
-    this.setState((state) => {
-      const items = this.toggleProperty(state.items, id, 'important');
-      return { items };
+  onServiceChange = () => {
+    this.setState(({ swapiService }) => {
+      const Service = swapiService instanceof SwapiService ?
+                        DummySwapiService : SwapiService;
+      return {
+        swapiService: new Service()
+      };
     });
   };
-
-  onDelete = (id) => {
-    this.setState((state) => {
-      const idx = state.items.findIndex((item) => item.id === id);
-      const items = [
-        ...state.items.slice(0, idx),
-        ...state.items.slice(idx + 1)
-      ];
-      return { items };
-    });
-  };
-
-  onFilterChange = (filter) => {
-    this.setState({ filter });
-  };
-
-  onSearchChange = (search) => {
-    this.setState({ search });
-  };
-
-  createItem(label) {
-    return {
-      id: ++this.maxId,
-      label,
-      important: false,
-      done: false
-    };
-  }
-
-  filterItems(items, filter) {
-    if (filter === 'all') {
-      return items;
-    } else if (filter === 'active') {
-      return items.filter((item) => (!item.done));
-    } else if (filter === 'done') {
-      return items.filter((item) => item.done);
-    }
-  }
-
-  searchItems(items, search) {
-    if (search.length === 0) {
-      return items;
-    }
-
-    return items.filter((item) => {
-      return item.label.toLowerCase().indexOf(search.toLowerCase()) > -1;
-    });
-  }
 
   render() {
-    const { items, filter, search } = this.state;
-    const doneCount = items.filter((item) => item.done).length;
-    const toDoCount = items.length - doneCount;
-    const visibleItems = this.searchItems(this.filterItems(items, filter), search);
+
+    const { isLoggedIn } = this.state;
 
     return (
-      <div className="todo-app">
-        <AppHeader toDo={toDoCount} done={doneCount}/>
+      <ErrorBoundry>
+        <SwapiServiceProvider value={this.state.swapiService} >
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
 
-        <div className="search-panel d-flex">
-          <SearchPanel
-            onSearchChange={this.onSearchChange}/>
+              <Switch>
+                <Route path="/"
+                       render={() => <h2>Welcome to StarDB</h2>}
+                       exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id"
+                       render={({ match }) => {
+                         const { id } = match.params;
+                         return <StarshipDetails itemId={id} />
+                       }}/>
 
-          <ItemStatusFilter
-            filter={filter}
-            onFilterChange={this.onFilterChange} />
-        </div>
+                <Route
+                  path="/login"
+                  render={() => (
+                    <LoginPage
+                      isLoggedIn={isLoggedIn}
+                      onLogin={this.onLogin}/>
+                  )}/>
 
-        <TodoList
-          items={ visibleItems }
-          onToggleImportant={this.onToggleImportant}
-          onToggleDone={this.onToggleDone}
-          onDelete={this.onDelete} />
+                <Route
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn} />
+                  )}/>
 
-        <ItemAddForm
-          onItemAdded={this.onItemAdded} />
-      </div>
+                <Route render={() => <h2>Page not found</h2>} />
+              </Switch>
+
+            </div>
+          </Router>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
     );
-  };
+  }
 }
